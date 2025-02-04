9df5b71531c11a82825efaa4b9f67d7d0d77a06f
Allen <allenjianggo@gmail.com>
2025-02-04 10:30:22 +0800

    fix: add cursor pointer style

--- resources/views/components/dropdown/button.blade.php
+++ resources/views/components/dropdown/button.blade.php
@@ -1,5 +1,5 @@
 <button
-  {{ $attributes->merge(['class' => 'flex w-full items-center rounded-md px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700', 'type' => 'button', 'role' => 'menuitem']) }}
+  {{ $attributes->merge(['class' => 'flex w-full items-center rounded-md px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer', 'type' => 'button', 'role' => 'menuitem']) }}
 >
   {{ $slot }}
 </button>
--- resources/views/components/dropdown/link.blade.php
+++ resources/views/components/dropdown/link.blade.php
@@ -1,4 +1,4 @@
 <a
-  {{ $attributes->merge(['class' => 'flex items-center rounded-md px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700', 'role' => 'menuitem', 'wire:navigate' => '']) }}>
+  {{ $attributes->merge(['class' => 'flex items-center rounded-md px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer', 'role' => 'menuitem', 'wire:navigate' => '']) }}>
   {{ $slot }}
 </a>
