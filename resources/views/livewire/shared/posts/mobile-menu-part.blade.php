c62475a7d5c68ddd97ec93208d7332eaf3cdcea5
Allen <allenjianggo@gmail.com>
2025-05-22 12:09:58 +0800

    refactor: update mobile menu style

--- resources/views/livewire/shared/posts/mobile-menu-part.blade.php
+++ resources/views/livewire/shared/posts/mobile-menu-part.blade.php
@@ -1,8 +1,6 @@
-<div
-  class="flex items-center justify-end gap-6 rounded-md text-base text-zinc-400 xl:hidden"
->
+<div class="isolate mb-6 inline-flex w-full items-center justify-end rounded-md text-base text-zinc-400 xl:hidden">
   <a
-    class="flex items-center text-green-600 hover:text-green-700 dark:text-green-500 dark:hover:text-green-600"
+    class="relative inline-flex items-center rounded-l-lg bg-zinc-50 px-4 py-2 text-zinc-400 ring-1 ring-inset ring-zinc-300 hover:bg-zinc-100 focus:z-10 dark:bg-zinc-800 dark:ring-zinc-600 dark:hover:bg-zinc-700"
     href="{{ route('posts.edit', ['id' => $postId]) }}"
   >
     <x-icons.pencil class="w-4" />
@@ -10,7 +8,7 @@
   </a>
 
   <button
-    class="flex items-center text-red-600 hover:text-red-700 dark:text-red-500 dark:hover:text-red-600"
+    class="relative -ml-px inline-flex cursor-pointer items-center rounded-r-lg bg-zinc-50 px-4 py-2 text-zinc-400 ring-1 ring-inset ring-zinc-300 hover:bg-zinc-100 focus:z-10 dark:bg-zinc-800 dark:ring-zinc-600 dark:hover:bg-zinc-700"
     type="button"
     wire:confirm="你確定要刪除文章嗎？（7 天之內可以還原）"
     wire:click="destroy({{ $postId }})"
