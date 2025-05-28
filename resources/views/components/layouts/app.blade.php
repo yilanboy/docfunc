75545054b3c43e42c6f62a6d4a9de7164b29422f
Allen <allenjianggo@gmail.com>
2025-05-28 13:36:22 +0800

    refactor: update font style

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -25,13 +25,6 @@
   {{-- Favicon --}}
   <link rel="icon" href="{{ asset('images/icon/logo.svg') }}" type="image/png">
 
-  {{-- Font --}}
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link
-    href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Noto+Sans+TC:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-    rel="stylesheet">
-
   @vite('resources/css/app.css')
 
   {{-- Cloudflare Turnstile --}}
