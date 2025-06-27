689f3a910bfb444a41dc6fd33542c3b1ea88e960
Allen <allenjianggo@gmail.com>
2025-06-27 13:19:19 +0800

    fix: remove redundant styling from mobile menu buttons

--- resources/views/livewire/shared/posts/mobile-menu-part.blade.php
+++ resources/views/livewire/shared/posts/mobile-menu-part.blade.php
@@ -1,6 +1,6 @@
-<div class="isolate mb-6 inline-flex w-full items-center justify-end rounded-md text-sm text-zinc-400 xl:hidden">
+<div class="isolate mb-6 inline-flex w-full items-center justify-end gap-0.5 rounded-md text-sm text-zinc-400 xl:hidden">
   <a
-    class="relative inline-flex items-center rounded-l-xl bg-zinc-50 px-4 py-2 text-zinc-400 ring-1 ring-inset ring-zinc-300 hover:bg-zinc-100 focus:z-10 dark:bg-zinc-800 dark:ring-zinc-600 dark:hover:bg-zinc-700"
+    class="relative inline-flex items-center rounded-l-xl bg-zinc-50 px-4 py-2 text-zinc-400 hover:bg-zinc-100 focus:z-10 dark:bg-zinc-800 dark:hover:bg-zinc-700"
     href="{{ route('posts.edit', ['id' => $postId]) }}"
   >
     <x-icons.pencil class="w-4" />
@@ -8,7 +8,7 @@
   </a>
 
   <button
-    class="relative -ml-px inline-flex cursor-pointer items-center rounded-r-xl bg-zinc-50 px-4 py-2 text-zinc-400 ring-1 ring-inset ring-zinc-300 hover:bg-zinc-100 focus:z-10 dark:bg-zinc-800 dark:ring-zinc-600 dark:hover:bg-zinc-700"
+    class="relative -ml-px inline-flex cursor-pointer items-center rounded-r-xl bg-zinc-50 px-4 py-2 text-zinc-400 hover:bg-zinc-100 focus:z-10 dark:bg-zinc-800 dark:hover:bg-zinc-700"
     type="button"
     wire:confirm="你確定要刪除文章嗎？（7 天之內可以還原）"
     wire:click="destroy({{ $postId }})"
