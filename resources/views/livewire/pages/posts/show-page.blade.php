c57dcb1ef19d1207d9acb1dd9e199ec39803a2e4
Allen <allenjianggo@gmail.com>
2025-07-07 10:56:06 +0800

    refactor: adjust post title background opacity for improved contrast and readability

--- resources/views/livewire/pages/posts/show-page.blade.php
+++ resources/views/livewire/pages/posts/show-page.blade.php
@@ -77,7 +77,7 @@
             <article>
               {{-- post title --}}
               <h1
-                class="dark:text-lividus-500 dark:from-lividus-800/60 bg-linear-to-r -mx-4 -mt-4 rounded-t-xl from-emerald-100/80 to-transparent px-4 py-6 text-4xl font-semibold leading-relaxed text-emerald-600"
+                class="dark:text-lividus-500 dark:from-lividus-800/60 bg-linear-to-r -mx-4 -mt-4 rounded-t-xl from-emerald-100/60 to-transparent px-4 py-6 text-4xl font-semibold leading-relaxed text-emerald-600"
               >
                 {{ $post->title }}
               </h1>
