baaffe9e7ba2bd529f0ad3eb7afb8f9b61e92c84
allenjiang <allenjianggo@gmail.com>
2026-01-29 23:18:03 +0800

    refactor: adjust layout positioning and simplify link component structure for improved readability and maintainability

--- resources/views/components/layouts/background.blade.php
+++ resources/views/components/layouts/background.blade.php
@@ -1,48 +1,48 @@
-<div class="absolute top-0 right-1/2 w-full h-screen translate-x-1/2 -z-10 max-w-[1920px]">
+<div class="absolute top-0 right-1/2 w-full h-screen translate-x-1/2 -z-10 max-w-480">
     <div id="background-right">
-        <div class="absolute left-[2%] top-[6rem] hidden md:block">
+        <div class="absolute left-[2%] top-24 hidden md:block">
             <x-icons.controller class="rotate-12 size-48 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[24%] top-[18rem] hidden lg:block">
+        <div class="absolute left-[24%] top-72 hidden lg:block">
             <x-icons.person-walking class="-rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[8%] top-[26rem] hidden lg:block">
+        <div class="absolute left-[8%] top-104 hidden lg:block">
             <x-icons.rocket-takeoff class="size-24 scale-x-[-1] text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[18%] top-[34rem] hidden md:block">
+        <div class="absolute left-[18%] top-136 hidden md:block">
             <x-icons.chat-heart class="-rotate-12 size-48 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[2%] top-[38rem] hidden lg:block">
+        <div class="absolute left-[2%] top-152 hidden lg:block">
             <x-icons.easel class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[8%] top-[48rem] hidden lg:block">
+        <div class="absolute left-[8%] top-192 hidden lg:block">
             <x-icons.globe-americas class="-rotate-12 size-24 scale-x-[-1] text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[24%] top-[4rem] hidden lg:block">
+        <div class="absolute left-[24%] top-16 hidden lg:block">
             <x-icons.hand-thumbs-up class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[38%] top-[10rem] hidden lg:block">
+        <div class="absolute left-[38%] top-40 hidden lg:block">
             <x-icons.lightbulb class="-rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[38%] top-[26rem] hidden lg:block">
+        <div class="absolute left-[38%] top-104 hidden lg:block">
             <x-icons.battery-charging class="-rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[40%] top-[40rem] hidden lg:block">
+        <div class="absolute left-[40%] top-160 hidden lg:block">
             <x-icons.music-note-beamed class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
     </div>
 
     <div id="background-right">
-        <div class="absolute right-[36%] top-[42rem] hidden lg:block">
+        <div class="absolute right-[36%] top-168 hidden lg:block">
             <x-icons.typescript
                 class="-rotate-12 size-24"
                 :rect-class-name="'fill-zinc-300/60 dark:fill-zinc-950/60'"
@@ -50,38 +50,39 @@
             />
         </div>
 
-        <div class="absolute right-[36%] top-[4rem] hidden lg:block">
+        <div class="absolute right-[36%] top-16 hidden lg:block">
             <x-icons.cloud class="rotate-12 size-48 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute right-[34%] top-[24rem] hidden lg:block">
+        <div class="absolute right-[34%] top-96 hidden lg:block">
             <x-icons.cup-hot class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute right-[46%] top-[30rem] hidden lg:block">
+        <div class="absolute right-[46%] top-120 hidden lg:block">
             <x-icons.clipboard-check class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
-        <div class="absolute right-[22%] top-[6rem] hidden lg:block">
+
+        <div class="absolute right-[22%] top-24 hidden lg:block">
             <x-icons.bug class="rotate-45 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute right-[4%] top-[12rem] hidden lg:block">
+        <div class="absolute right-[4%] top-48 hidden lg:block">
             <x-icons.folder-open class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute right-[14%] top-[18rem] hidden md:block">
+        <div class="absolute right-[14%] top-72 hidden md:block">
             <x-icons.floppy class="-rotate-12 size-48 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute right-[4%] top-[32rem] hidden lg:block">
+        <div class="absolute right-[4%] top-128 hidden lg:block">
             <x-icons.cpu class="-rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute right-[20%] top-[40rem] hidden lg:block">
+        <div class="absolute right-[20%] top-160 hidden lg:block">
             <x-icons.terminal class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute right-[4%] top-[44rem] hidden md:block">
+        <div class="absolute right-[4%] top-176 hidden md:block">
             <x-icons.php
                 class="rotate-12 size-48"
                 :pathClassName="'fill-zinc-300/60 dark:fill-zinc-950/60'"
--- resources/views/components/skew-underline-link.blade.php
+++ resources/views/components/skew-underline-link.blade.php
@@ -1,29 +1,30 @@
-{{-- prettier-ignore-start --}}
 @props([
     'link',
     'icon' => '',
     'selected' => false
 ])
-{{-- prettier-ignore-end --}}
 
-<a
-    class="inline-block relative justify-center items-center px-1 w-auto h-auto text-lg rounded-sm transition-all duration-300 cursor-pointer hover:-rotate-3 outline-hidden group text-zinc-900/80 dark:text-zinc-50 active:outline-hidden"
-    href="{{ $link }}"
-    {{ $attributes }}
-    wire:navigate
->
-  <span class="flex relative z-20 justify-center items-center">
-    @if (empty($icon))
-          <x-icons.home class="w-5" />
-      @else
-          <div class="w-5">{!! $icon !!}</div>
-      @endif
+<div class="cursor-pointer group">
+    <a
+        class="inline-block relative justify-center items-center px-1 w-auto h-auto text-lg rounded-sm transition-all duration-300 group-hover:-rotate-3 outline-hidden text-zinc-900/80 dark:text-zinc-50 active:outline-hidden"
+        href="{{ $link }}"
+        {{ $attributes }}
+        wire:navigate
+    >
+        <span class="flex relative z-20 justify-center items-center">
+            @if (empty($icon))
+                <x-icons.home class="w-5" />
+            @else
+                <div class="w-5">{!! $icon !!}</div>
+            @endif
 
-    <span class="ml-2">{{ $slot }}</span>
-  </span>
-    <span @class([
-      'absolute bottom-0 left-0 z-10 h-2 w-0 skew-x-12 bg-emerald-400 dark:bg-lividus-600',
-      'transition-all duration-300 ease-out group-hover:w-full' => !$selected,
-      'w-full' => $selected,
-  ])></span>
-</a>
+            <span class="ml-2">{{ $slot }}</span>
+        </span>
+
+        <span @class([
+            'absolute bottom-0 left-0 z-10 h-2 w-0 skew-x-12 bg-emerald-400 dark:bg-lividus-600',
+            'transition-all duration-300 ease-out group-hover:w-full' => !$selected,
+            'w-full' => $selected,
+        ])></span>
+    </a>
+</div>
