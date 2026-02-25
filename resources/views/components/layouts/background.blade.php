3c7dc8f4ede620b52bf879cdd0c82df7e8389cb3
Allen <allenjianggo@gmail.com>
2026-02-25 15:41:41 +0800

    chore: adjust icon positions and add new icons in background layout for better design balance

--- resources/views/components/layouts/background.blade.php
+++ resources/views/components/layouts/background.blade.php
@@ -1,91 +1,91 @@
 <div class="fixed top-0 right-1/2 w-full h-screen translate-x-1/2 -z-10 max-w-480">
     <div id="background-left">
-        <div class="absolute left-[2%] top-24 hidden md:block">
-            <x-icons.controller class="rotate-12 size-42 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[20%] top-72 hidden lg:block">
-            <x-icons.person-walking class="-rotate-12 size-36 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[8%] top-104 hidden lg:block">
-            <x-icons.rocket-takeoff class="size-24 scale-x-[-1] text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[20%] top-136 hidden md:block">
-            <x-icons.chat-heart class="-rotate-12 size-42 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[2%] top-152 hidden lg:block">
-            <x-icons.easel class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[8%] top-196 hidden lg:block">
-            <x-icons.globe-americas class="-rotate-12 size-36 scale-x-[-1] text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
         <div class="absolute left-[22%] top-16 hidden lg:block">
             <x-icons.hand-thumbs-up class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[38%] top-40 hidden lg:block">
+        <div class="absolute left-[4%] top-40 hidden md:block">
+            <x-icons.controller class="rotate-12 size-42 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute left-[36%] top-64 hidden lg:block">
             <x-icons.lightbulb class="-rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[38%] top-100 hidden lg:block">
+        <div class="absolute left-[16%] top-88 hidden lg:block">
+            <x-icons.person-walking class="-rotate-12 size-36 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute left-[36%] top-112 hidden lg:block">
             <x-icons.battery-charging class="-rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
 
-        <div class="absolute left-[38%] top-180 hidden lg:block">
+        <div class="absolute left-[4%] top-132 hidden lg:block">
+            <x-icons.rocket-takeoff class="size-24 scale-x-[-1] text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute left-[20%] top-156 hidden md:block">
+            <x-icons.chat-heart class="-rotate-12 size-42 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute left-[36%] top-176 hidden lg:block">
+            <x-icons.easel class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute left-[6%] top-200 hidden lg:block">
+            <x-icons.globe-americas class="-rotate-12 size-36 scale-x-[-1] text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute left-[28%] top-224 hidden lg:block">
             <x-icons.music-note-beamed class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
         </div>
     </div>
 
     <div id="background-right">
-        <div class="absolute right-[36%] top-196 hidden lg:block">
+        <div class="absolute right-[22%] top-20 hidden lg:block">
+            <x-icons.bug class="rotate-45 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute right-[4%] top-44 hidden lg:block">
+            <x-icons.folder-open class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute right-[36%] top-40 hidden lg:block">
+            <x-icons.cloud class="rotate-12 size-42 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute right-[14%] top-92 hidden md:block">
+            <x-icons.floppy class="-rotate-12 size-42 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute right-[36%] top-116 hidden lg:block">
+            <x-icons.cup-hot class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute right-[4%] top-136 hidden lg:block">
+            <x-icons.cpu class="-rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute right-[24%] top-160 hidden lg:block">
+            <x-icons.terminal class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute right-[38%] top-184 hidden lg:block">
+            <x-icons.clipboard-check class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
+        </div>
+
+        <div class="absolute right-[6%] top-208 hidden md:block">
+            <x-icons.php
+                class="rotate-12 size-42"
+                :pathClassName="'fill-zinc-300/60 dark:fill-zinc-950/60'"
+            />
+        </div>
+
+        <div class="absolute right-[30%] top-232 hidden lg:block">
             <x-icons.typescript
                 class="-rotate-12 size-24"
                 :rect-class-name="'fill-zinc-300/60 dark:fill-zinc-950/60'"
                 :path-class-name="'fill-zinc-200 dark:fill-zinc-900'"
-            />
-        </div>
-
-        <div class="absolute right-[36%] top-16 hidden lg:block">
-            <x-icons.cloud class="rotate-12 size-42 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute right-[34%] top-96 hidden lg:block">
-            <x-icons.cup-hot class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute right-[46%] top-140 hidden lg:block">
-            <x-icons.clipboard-check class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute right-[22%] top-16 hidden lg:block">
-            <x-icons.bug class="rotate-45 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute right-[4%] top-36 hidden lg:block">
-            <x-icons.folder-open class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute right-[16%] top-72 hidden md:block">
-            <x-icons.floppy class="-rotate-12 size-42 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute right-[4%] top-128 hidden lg:block">
-            <x-icons.cpu class="-rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute right-[20%] top-160 hidden lg:block">
-            <x-icons.terminal class="rotate-12 size-24 text-zinc-300/60 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute right-[4%] top-196 hidden md:block">
-            <x-icons.php
-                class="rotate-12 size-42"
-                :pathClassName="'fill-zinc-300/60 dark:fill-zinc-950/60'"
             />
         </div>
     </div>
