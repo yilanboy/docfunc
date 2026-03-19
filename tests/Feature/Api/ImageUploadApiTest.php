ed709585d0bd21f8a6407bcec11c85317e684d80
allenjiang <allenjianggo@gmail.com>
2026-03-19 09:50:23 +0800

    test: expand image upload API test coverage with additional validation scenarios

--- tests/Feature/Api/ImageUploadApiTest.php
+++ tests/Feature/Api/ImageUploadApiTest.php
@@ -5,36 +5,87 @@ use Illuminate\Support\Facades\Storage;
 
 use function Pest\Laravel\post;
 
-beforeEach(fn () => Storage::fake());
+beforeEach(fn() => Storage::fake());
 
-test('users who are not logged in cannot upload images', function () {
-    post(route('images.store'), [
-        'upload' => UploadedFile::fake()->image('photo.jpg')->size(100),
-    ])
-        ->assertStatus(302)
-        ->assertRedirect(route('login'));
+describe('image upload api', function () {
+    test('users who are not logged in cannot upload images', function () {
+        post(route('images.store'), [
+            'upload' => UploadedFile::fake()->image('photo.jpg')->size(100),
+        ])
+            ->assertStatus(302)
+            ->assertRedirect(route('login'));
 
-    Storage::disk()->assertDirectoryEmpty('images');
-});
+        Storage::disk()->assertDirectoryEmpty('images');
+    });
 
-test('logged-in users can upload images', function () {
-    loginAsUser();
+    test('logged-in users can upload images', function () {
+        loginAsUser();
 
-    $file = UploadedFile::fake()->image('photo.jpg')->size(100);
+        $file = UploadedFile::fake()->image('photo.jpg')->size(100);
 
-    post(route('images.store'), [
-        'upload' => $file,
-    ])->assertStatus(200);
+        post(route('images.store'), [
+            'upload' => $file,
+        ])->assertStatus(200)
+            ->assertJsonStructure(['url']);
 
-    expect(Storage::disk()->allFiles())->not->toBeEmpty();
-});
+        expect(Storage::disk()->allFiles())->not->toBeEmpty();
+    });
 
-test('the size of the uploaded image must be less than 1024 kb', function () {
-    loginAsUser();
+    test('the upload field is required', function () {
+        loginAsUser();
 
-    post(route('images.store'), [
-        'upload' => UploadedFile::fake()->image('photo.jpg')->size(1025),
-    ])->assertStatus(413);
+        post(route('images.store'), [])
+            ->assertStatus(413)
+            ->assertJsonStructure(['error' => ['message']]);
 
-    Storage::disk()->assertDirectoryEmpty('images');
+        Storage::disk()->assertDirectoryEmpty('images');
+    });
+
+    test('the width of the uploaded image must be less than 1200 px', function () {
+        loginAsUser();
+
+        post(route('images.store'), [
+            'upload' => UploadedFile::fake()->image('photo.jpg', 1201, 100),
+        ])
+            ->assertStatus(413)
+            ->assertJsonStructure(['error' => ['message']]);
+
+        Storage::disk()->assertDirectoryEmpty('images');
+    });
+
+    test('the height of the uploaded image must be less than 1200 px', function () {
+        loginAsUser();
+
+        post(route('images.store'), [
+            'upload' => UploadedFile::fake()->image('photo.jpg', 100, 1201),
+        ])
+            ->assertStatus(413)
+            ->assertJsonStructure(['error' => ['message']]);
+
+        Storage::disk()->assertDirectoryEmpty('images');
+    });
+
+    test('the size of the uploaded image must be less than 1024 kb', function () {
+        loginAsUser();
+
+        post(route('images.store'), [
+            'upload' => UploadedFile::fake()->image('photo.jpg')->size(1025),
+        ])
+            ->assertStatus(413)
+            ->assertJsonStructure(['error' => ['message']]);
+
+        Storage::disk()->assertDirectoryEmpty('images');
+    });
+
+    test('the uploaded image must be an image', function () {
+        loginAsUser();
+
+        post(route('images.store'), [
+            'upload' => UploadedFile::fake()->create('document.pdf', 100),
+        ])
+            ->assertStatus(413)
+            ->assertJsonStructure(['error' => ['message']]);
+
+        Storage::disk()->assertDirectoryEmpty('images');
+    });
 });
