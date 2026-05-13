660fa1d0c08416811898713897c721be0977887e
Allen <allenjianggo@gmail.com>
2026-05-13 15:21:49 +0800

    chore: update dependencies and background

--- resources/views/components/layouts/background.blade.php
+++ resources/views/components/layouts/background.blade.php
@@ -1,92 +1,95 @@
-<div class="fixed top-0 right-1/2 w-full h-screen translate-x-1/2 -z-10 max-w-480">
-    <div id="background-left">
-        <div class="absolute left-[22%] top-16 hidden lg:block">
-            <x-icons.hand-thumbs-up class="rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
+<div class="fixed top-0 right-1/2 w-full h-screen translate-x-1/2 pointer-events-none select-none -z-10 max-w-480">
+    <div
+        aria-hidden="true"
+        class="absolute bottom-32 left-[3%] hidden flex-col gap-10 font-jetbrains-mono text-base leading-none font-light tracking-widest text-zinc-400/40 lg:flex dark:text-zinc-600/40"
+    >
+        <span>01</span>
+        <span>02</span>
+        <span>03</span>
+        <span>04</span>
+        <span>05</span>
+    </div>
+
+    <div
+        aria-hidden="true"
+        class="absolute bottom-12 left-[3%] hidden flex-col gap-4 font-jetbrains-mono text-sm leading-none text-zinc-500/60 lg:flex dark:text-zinc-500/60"
+    >
+        <div class="flex gap-3 items-center font-semibold">
+            <span>Laravel&nbsp;13</span>
+            <span class="text-zinc-500/40 dark:text-zinc-500/40">·</span>
+            <span>Livewire&nbsp;4</span>
         </div>
 
-        <div class="absolute left-[4%] top-40 hidden md:block">
-            <x-icons.controller class="rotate-12 size-42 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[36%] top-64 hidden lg:block">
-            <x-icons.lightbulb class="-rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[16%] top-88 hidden lg:block">
-            <x-icons.person-walking class="-rotate-12 size-36 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[36%] top-112 hidden lg:block">
-            <x-icons.battery-charging class="-rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[4%] top-132 hidden lg:block">
-            <x-icons.rocket-takeoff class="size-24 scale-x-[-1] text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[20%] top-156 hidden md:block">
-            <x-icons.chat-heart class="-rotate-12 size-42 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[36%] top-176 hidden lg:block">
-            <x-icons.easel class="rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[6%] top-200 hidden lg:block">
-            <x-icons.globe-americas class="-rotate-12 size-36 scale-x-[-1] text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
-
-        <div class="absolute left-[28%] top-224 hidden lg:block">
-            <x-icons.music-note-beamed class="rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
+        <div>
+            &lt;?php declare(strict_types=1);
         </div>
     </div>
 
-    <div id="background-right">
-        <div class="absolute right-[22%] top-20 hidden lg:block">
-            <x-icons.bug class="rotate-45 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
+    <div
+        aria-hidden="true"
+        class="absolute bottom-16 left-[1.5%] hidden h-[35vh] w-px bg-gradient-to-t from-transparent via-zinc-300/40 to-transparent lg:block dark:via-zinc-700/40"
+    ></div>
 
-        <div class="absolute right-[4%] top-44 hidden lg:block">
-            <x-icons.folder-open class="rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
+    <div class="absolute top-[5vh] right-[20%] hidden lg:block">
+        <x-icons.livewire class="-rotate-6 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
 
-        <div class="absolute right-[36%] top-40 hidden lg:block">
-            <x-icons.cloud class="rotate-12 size-42 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
+    <div class="absolute top-[6vh] right-[2%] hidden lg:block">
+        <x-icons.terminal class="rotate-6 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
 
-        <div class="absolute right-[14%] top-92 hidden md:block">
-            <x-icons.floppy class="-rotate-12 size-42 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
+    <div class="absolute top-[13vh] right-[11%] hidden lg:block">
+        <x-icons.svelte class="-rotate-12 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
 
-        <div class="absolute right-[36%] top-116 hidden lg:block">
-            <x-icons.cup-hot class="rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
+    <div class="absolute top-[20vh] right-[20%] hidden lg:block">
+        <x-icons.person-walking class="rotate-12 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
 
-        <div class="absolute right-[4%] top-136 hidden lg:block">
-            <x-icons.cpu class="-rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
+    <div class="absolute top-[27vh] right-[2%] hidden lg:block">
+        <x-icons.php
+            class="-rotate-6 size-20"
+            :path-class-name="'fill-zinc-300/40 dark:fill-zinc-700/40'"
+        />
+    </div>
 
-        <div class="absolute right-[24%] top-160 hidden lg:block">
-            <x-icons.terminal class="rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
+    <div class="absolute top-[34vh] right-[11%] hidden lg:block">
+        <x-icons.github class="-rotate-12 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
 
-        <div class="absolute right-[38%] top-184 hidden lg:block">
-            <x-icons.clipboard-check class="rotate-12 size-24 text-zinc-300/40 dark:text-zinc-950/80" />
-        </div>
+    <div class="absolute top-[41vh] right-[20%] hidden lg:block">
+        <x-icons.folder-open class="rotate-6 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
 
-        <div class="absolute right-[6%] top-208 hidden md:block">
-            <x-icons.php
-                class="rotate-12 size-42"
-                :pathClassName="'fill-zinc-300/40 dark:fill-zinc-950/60'"
-            />
-        </div>
+    <div class="absolute top-[48vh] right-[2%] hidden lg:block">
+        <x-icons.lightbulb class="-rotate-6 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
 
-        <div class="absolute right-[30%] top-232 hidden lg:block">
-            <x-icons.typescript
-                class="-rotate-12 size-24"
-                :rect-class-name="'fill-zinc-300/40 dark:fill-zinc-950/60'"
-                :path-class-name="'fill-zinc-200 dark:fill-zinc-900'"
-            />
-        </div>
+    <div class="absolute top-[55vh] right-[11%] hidden lg:block">
+        <x-icons.typescript
+            class="rotate-12 size-20"
+            :rect-class-name="'fill-zinc-300/40 dark:fill-zinc-700/40'"
+            :path-class-name="'fill-zinc-200 dark:fill-zinc-900'"
+        />
+    </div>
+
+    <div class="absolute top-[62vh] right-[20%] hidden lg:block">
+        <x-icons.bug class="rotate-12 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
+
+    <div class="absolute top-[69vh] right-[2%] hidden lg:block">
+        <x-icons.rocket-takeoff class="-rotate-6 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
+
+    <div class="absolute top-[76vh] right-[11%] hidden lg:block">
+        <x-icons.cloud class="-rotate-12 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
+
+    <div class="absolute right-[20%] bottom-[7vh] hidden lg:block">
+        <x-icons.cpu class="-rotate-6 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
+    </div>
+
+    <div class="absolute right-[2%] bottom-[7vh] hidden lg:block">
+        <x-icons.floppy class="rotate-12 size-20 text-zinc-300/40 dark:text-zinc-700/40" />
     </div>
 </div>
